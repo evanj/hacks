06fd77f691d91b5a65d1e362067b709e6274e035
Evan Jones <ej@evanjones.ca>
2020-12-12 13:58:53 -0500

    postgrestmp: start a temporary postgres shell

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 I often write little throwaway tools when I am working. Occasionally, I write the same tool more than once, at which point I try to turn it into a more serious thing. This is my collection of them. These are for me, but I'm making this public and open source in case they are helpful to others.
 
 
-## timeparse
+## timeparse: parse a time into local, UTC, and unix times
 
-This command line utility attempts to guess what format a time is in, and converts it to local time, UTC, and unix timestamps. Example:
+Attempts to guess what format a time is in, and converts it to local time, UTC, and unix timestamps. Example:
 
 ```
 $ go run ./timeparse 'Sat Dec 12 13:27:44 EST 2020'
@@ -13,3 +13,32 @@ Sat Dec 12 13:27:44 EST 2020 (unix_date)
   LOCAL: 2020-12-12T13:27:44-05:00  UTC: 2020-12-12T18:27:44Z  UNIX EPOCH: 1607797664
 ```
 
+## postgrestmp: start a temporary postgres shell
+
+Creates a new Postgres database in a temporary directory, then runs the psql command line utility to connect to it. When psql exits, the database is deleted. Example:
+
+```
+$ go run ./postgrestmp 
+initializing temporary postgres database in /var/folders/s_/cmjk7jmx445cbktl7q_p2z0r0000gn/T/postgrestmp_707622637 ...
+
+[... postgres output omitted ...]
+
+starting psql ...
+2020-12-12 13:56:45.520 EST [4446] LOG:  database system was shut down at 2020-12-12 13:56:45 EST
+2020-12-12 13:56:45.523 EST [4445] LOG:  database system is ready to accept connections
+psql (13.1)
+Type "help" for help.
+
+postgres=# create table hello (id integer, value text);
+CREATE TABLE
+postgres=# insert into hello values (1, 'a'), (1, 'b');
+INSERT 0 2
+postgres=# select id, count(*) from hello group by id;
+ id | count 
+----+-------
+  1 |     2
+(1 row)
+
+postgres=# \q
+2020-12-12 13:57:24.747 EST [4445] LOG:  database system is shut down
+```
---
+++ postgrestmp/postgrestmp.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func initializePostgresDir(dbDir string) error {
+	cmd := exec.Command("initdb", "-D", dbDir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+type postgresProcess struct {
+	proc  *exec.Cmd
+	dbDir string
+}
+
+const pgSocketName = "/tmp/.s.PGSQL.5432"
+
+func startPostgres(dbDir string) (*postgresProcess, error) {
+	// By default Postgres puts its Unix-domain socket in /tmp; "-k ." puts it in the data dir.
+	// however, then we get "socket name too long" because the absolute path to the socket
+	// can't exceed 100 characters
+	proc := exec.Command("postgres", "-D", dbDir)
+	proc.Stdout = os.Stdout
+	proc.Stderr = os.Stderr
+	err := proc.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	// poll for the socket to be created
+	const maxPolls = 40
+	// on my laptop, 15ms was SOMETIMES enough; 20ms was almost always enough time
+	const pollSleep = 20 * time.Millisecond
+	started := false
+	for i := 0; i < maxPolls; i++ {
+		time.Sleep(pollSleep)
+
+		_, err = os.Stat(pgSocketName)
+		if err == nil {
+			started = true
+			break
+		}
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+	}
+	if !started {
+		time.Sleep(time.Hour)
+		proc.Process.Kill()
+		return nil, errors.New("failed to find Postgres UNIX domain socket " + pgSocketName)
+	}
+
+	return &postgresProcess{proc, dbDir}, nil
+}
+
+func (p *postgresProcess) Close() error {
+	if p.proc == nil {
+		return nil
+	}
+
+	proc := p.proc
+	p.proc = nil
+	// SIGINT = fast shutdown: terminates all child processes
+	err := proc.Process.Signal(syscall.SIGINT)
+	if err != nil {
+		return err
+	}
+	return proc.Wait()
+}
+
+func main() {
+	dir, err := ioutil.TempDir("", "postgrestmp_")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	fmt.Printf("initializing temporary postgres database in %s ...\n", dir)
+	err = initializePostgresDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("starting postgres in the background ...\n")
+	proc, err := startPostgres(dir)
+	if err != nil {
+		panic(err)
+	}
+	defer proc.Close()
+
+	fmt.Printf("starting psql ...\n")
+	psql := exec.Command("psql", "postgres")
+	psql.Stdin = os.Stdin
+	psql.Stdout = os.Stdout
+	err = psql.Run()
+	if err != nil {
+		panic(err)
+	}
+}
