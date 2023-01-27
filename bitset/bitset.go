86b8e015933c201681161c3ad7d6fa2b76436f05
Evan Jones <ej@evanjones.ca>
2023-01-27 10:13:10 -0500

    bitset: Fix M1 Max Arm64 performance

--- bitset/bitset.go
+++ bitset/bitset.go
@@ -1,6 +1,8 @@
 // Package bitset implements a dense bit set with a similar API as roaring bitmaps.
 // This is written as an experiment and is incomplete. Use bits-and-blooms instead,
 // or Roaring Bitmaps if you want compressed sparsed bit sets.
+//
+
 package bitset
 
 import (
@@ -11,46 +13,51 @@ import (
 bitSet is a fixed-size set of bits, encoded as an array of uint64. It has a similar interface
 as Roaring Bitmaps. In my quick-and-dirty benchmark, bitSet is faster, but uses more
 memory unless >= ~25% of the bits are set. The bits-and-blooms implementation is more complete,
-but has similar performance to this implementation.
+but has similar performance to this implementation. I had to do some minor tweaks to make the
+performance the same on the M1 Max.
 
-See: https://pkg.go.dev/github.com/RoaringBitmap/roaring
-See:
+Roaring Bitmaps: https://pkg.go.dev/github.com/RoaringBitmap/roaring
+bits-and-blooms: https://pkg.go.dev/github.com/bits-and-blooms/bitset
 
 Benchmark results with Go 1.19 on an Mac M1 Max, showing that dense is faster but uses more memory.
 This also shows that bits-and-blooms appears to be faster.
 
-	BenchmarkBitSet/bitset_set_and_iterate_p01-10         	   11330	    105355 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p01-10  	   14216	     84549 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p01-10        	    2143	    560578 ns/op	   33576 B/op	      26 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_p10-10         	    1317	    899670 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p10-10  	    1662	    722709 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p10-10        	     435	   2746712 ns/op	   66808 B/op	      43 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_p25-10         	     537	   2222508 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p25-10  	     669	   1781710 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p25-10        	     159	   7484694 ns/op	  133272 B/op	      76 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p01-10        81428 ns/op    131073 B/op        1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p01-10              79303 ns/op    131073 B/op        1 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p01-10       84585 ns/op    131104 B/op        2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p01-10            562687 ns/op     33576 B/op       26 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p10-10       675344 ns/op    131072 B/op        1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p10-10             680148 ns/op    131072 B/op        1 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p10-10      724586 ns/op    131104 B/op        2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p10-10           2772311 ns/op     66808 B/op       43 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p25-10      1660769 ns/op    131072 B/op        1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p25-10            1679911 ns/op    131072 B/op        1 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p25-10     1836730 ns/op    131104 B/op        2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p25-10           7627645 ns/op    133272 B/op       76 allocs/op
 
 Results on an Intel 11th Gen Core i5-1135G7 (TigerLake) shows that this is competitive with
 bits-and-blooms.
 
 	cpu: 11th Gen Intel(R) Core(TM) i5-1135G7 @ 2.40GHz
-	BenchmarkBitSet/bitset_set_and_iterate_no_it_p01-8         	   23568	    148285 ns/op	  131072 B/op	       1 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_p01-8               	   25155	    144220 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p01-8        	   28768	    137650 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p01-8              	    6109	    568225 ns/op	   33576 B/op	      26 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_no_it_p10-8         	    3720	    943776 ns/op	  131072 B/op	       1 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_p10-8               	    3747	    958302 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p10-8        	    3870	    966873 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p10-8              	    1138	   3391472 ns/op	   66808 B/op	      43 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_no_it_p25-8         	    1540	   2386860 ns/op	  131072 B/op	       1 allocs/op
-	BenchmarkBitSet/bitset_set_and_iterate_p25-8               	    1428	   2476936 ns/op	  131112 B/op	       3 allocs/op
-	BenchmarkBitSet/bitsandblooms_set_and_iterate_p25-8        	    1530	   2440958 ns/op	  131104 B/op	       2 allocs/op
-	BenchmarkBitSet/roaring_set_and_iterate_p25-8              	     427	   8397258 ns/op	  133272 B/op	      76 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p01-8        148285 ns/op    131072 B/op         1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p01-8              144220 ns/op    131112 B/op         3 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p01-8       137650 ns/op    131104 B/op         2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p01-8             568225 ns/op     33576 B/op        26 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p10-8        943776 ns/op    131072 B/op         1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p10-8              958302 ns/op    131112 B/op         3 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p10-8       966873 ns/op    131104 B/op         2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p10-8            3391472 ns/op     66808 B/op        43 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_no_it_p25-8       2386860 ns/op    131072 B/op         1 allocs/op
+	BenchmarkBitSet/bitset_set_and_iterate_p25-8             2476936 ns/op    131112 B/op         3 allocs/op
+	BenchmarkBitSet/bitsandblooms_set_and_iterate_p25-8      2440958 ns/op    131104 B/op         2 allocs/op
+	BenchmarkBitSet/roaring_set_and_iterate_p25-8            8397258 ns/op    133272 B/op        76 allocs/op
 */
 type bitSet struct {
 	bits []uint64
 }
 
 const bitSetBitSize = 64
+const bitSetBitSizeLog2 = 6
 
 // newBitSet returns a new bitSet that stores size bits. The supported indexes are [0, size).
 func newBitSet(size int) *bitSet {
@@ -63,7 +70,9 @@ func newBitSet(size int) *bitSet {
 func bitSetIndexes(index int) (uintIndex int, bitIndex int) {
 	const bitSetBitMask = bitSetBitSize - 1
 
-	uintIndex = index / bitSetBitSize
+	// index / bitSetBitSize should generate the same code
+	// however on ARM64, it does not, and it seems to matter on the M1 Max.
+	uintIndex = index >> bitSetBitSizeLog2
 	bitIndex = index & bitSetBitMask
 	return uintIndex, bitIndex
 }
@@ -95,21 +104,32 @@ func (b *bitSet) contains(index int) bool {
 
 // nextSet returns the set bit >= index. It returns lenBits() if there is no set bit >= index.
 func (b *bitSet) nextSet(index int) int {
+	// special case the first int: removes a few operations from the for loop
+	// improves performance on M1 ARM64; no real diff on x86
 	uintIndex, bitIndex := bitSetIndexes(index)
+	if uintIndex >= len(b.bits) {
+		return b.lenBits()
+	}
+	bitsUint := b.bits[uintIndex]
+	bitsUint >>= bitIndex
 
+	// testing if bitsUint != 0 does not seem to change performance
+	zeros := bits.TrailingZeros64(bitsUint)
+	if zeros < bitSetBitSize {
+		// found a set bit!
+		return zeros + index
+	}
+
+	// check the next integers
+	uintIndex++
 	for uintIndex < len(b.bits) {
 		bitsUint := b.bits[uintIndex]
-		bitsUint >>= bitIndex
 		zeros := bits.TrailingZeros64(bitsUint)
 		if zeros < bitSetBitSize {
 			// found a set bit!
-			return zeros + index
+			return zeros + uintIndex*bitSetBitSize
 		}
-
-		// all zeros: nothing is set: check the next uint
 		uintIndex++
-		bitIndex = 0
-		index = uintIndex * bitSetBitSize
 	}
 	return b.lenBits()
 }
